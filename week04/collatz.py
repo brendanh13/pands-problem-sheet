6bb5afe6d642d73ab0e2f35caf3f5d1bd40738b9
brendanh13 <brendanheeney@gmail.com>
2022-02-21 22:52:09 +0000

    Week 4 Problem Sheet

---
+++ week04/collatz.py
@@ -0,0 +1,23 @@
+# Write a program that asks the user to input any 
+# positive integer and outputs the successive values of the following calculation.
+
+# At each step calculate the next value by taking the 
+# current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+
+# Have the program end if the current value is one.
+
+
+
+# Author = Brendan Heeney
+
+
+
+n = int(input("Please enter a positive integer: "))
+print(n, end=" ")
+
+while n != 1:                   # we know that we exit when the number is 1 so we set our number to be not equal to 1
+    if n % 2 == 0:              # we check if the number is even
+        n = n / 2               # if it is true; we divide it by 2
+    else:
+        n = 3 * n + 1           # else we multiply by 3 and add 1
+    print(int(n), end=" ")      
