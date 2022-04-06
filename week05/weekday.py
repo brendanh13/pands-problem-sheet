0ad0a5c8d078818463b4b9f69391f1d04178a20a
brendanh13 <brendanheeney@gmail.com>
2022-04-06 23:47:38 +0100

    tidying up problem sheets

--- week05/weekday.py
+++ week05/weekday.py
@@ -1,22 +1,12 @@
-# Problem Sheet for Week 5
-
-# Author: Brendan Heeney
+# Week 5 - Weekly Task
 
 # Write a program that outputs whether or not today is a weekday.
 
-
+# Author: Brendan Heeney
 
 from datetime import datetime
 
-dayOfWeek = datetime.today().weekday()   # found on internet - this tells us which day of the week today is; 0 = Mon, 1 = Tue etc
-
-daysOfWeek = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")  # not now required for this exercise
-
-#weekDays = daysOfWeek[0:5]
-
-#print(weekDays)
-#print(dayOfWeek)  # told me today = 5 which is Saturday (as I write this on a Saturday!)
-
+dayOfWeek = datetime.today().weekday()   # this tells us which day of the week today is; 0 = Mon, 1 = Tue etc
 
 if dayOfWeek < 5:     # this will be a number from 0 - 6 depending on which day of the week it is
     print("Yes, unfortunately today is a weekday.")
